bf2c14d9cfebab4bc401f217bdcf6b8d4a869399
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-05 16:38:33 -0800

    Update deshitifier.js

--- deshitifier.js
+++ deshitifier.js
@@ -1,5 +1,5 @@
 document.getElementById("stories_pagelet_rhc").remove();
-document.getElementById("game_rhc_tabs").remove();
+document.getElementById("pagelet_games_rhc").remove();
 document.getElementById("pagelet_ego_pane").remove();
 document.getElementById("appsNav").remove();
 document.getElementById("pinnedNav").remove();
