e2b6e1593166230f8e25e6cf635d6e3cb99fa664
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-05 16:35:07 -0800

    Create deshitifier.js

---
+++ deshitifier.js
@@ -0,0 +1,6 @@
+document.getElementById("stories_pagelet_rhc").remove();
+document.getElementById("game_rhc_tabs").remove();
+document.getElementById("pagelet_ego_pane").remove();
+document.getElementById("appsNav").remove();
+document.getElementById("pinnedNav").remove();
+document.getElementById("pagelet_reminders").remove();
