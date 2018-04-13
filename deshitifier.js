dcf1587c7e5437c41572ed6104eb0ccea50567cb
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-04-12 20:28:16 -0700

    Update deshitifier.js

--- deshitifier.js
+++ deshitifier.js
@@ -1,6 +1,11 @@
-document.getElementById("stories_pagelet_rhc").remove();
-document.getElementById("pagelet_games_rhc").remove();
-document.getElementById("pagelet_ego_pane").remove();
-document.getElementById("appsNav").remove();
-document.getElementById("pinnedNav").remove();
-document.getElementById("pagelet_reminders").remove();
+try {document.getElementById("stories_pagelet_rhc").remove();} catch(e) {}
+try {document.getElementById("pagelet_games_rhc").remove();} catch(e) {}
+try {document.getElementById("pagelet_ego_pane").remove();} catch(e) {}
+try {document.getElementById("appsNav").remove();} catch(e) {}
+try {document.getElementById("pinnedNav").remove();} catch(e) {}
+try {document.getElementById("pagelet_reminders").remove();} catch(e) {}
+
+try {document.getElementById("u_0_t").remove();} catch(e) {}
+try {document.getElementById("pagelet_rhc_footer").remove();} catch(e) {}
+try {document.getElementById("universalNav").remove();} catch(e) {}
+try {document.getElementById("createNav").remove();} catch(e) {}
